171d0653c7f5d00333e991f24a8efbfb73ad557a
Anderson <anderg029610@outlook.com>
2026-07-08 11:39:52 -0500

    Agregando yazi, sin problema acá

---
+++ yazi/init.lua
@@ -0,0 +1,4 @@
+require("chronos"):setup({ enable = true, notify_mode = "summary" })
+require("folder-rules"):setup()
+require("starship"):setup()
+require("recycle-bin"):setup({ trash_dir = "/home/ander/.local/share/Trash/" })
